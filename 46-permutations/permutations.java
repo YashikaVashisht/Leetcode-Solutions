0c66a7573486e3478202e930096faad6092b8674
YashikaVashisht <137279767+YashikaVashisht@users.noreply.github.com>
2025-09-25 16:42:38 +0530

    Time: 1 ms (92.82%) | Memory: 44.8 MB (30.63%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void helper(int[] nums , boolean[] vis, List<List<Integer>> ans, List<Integer> res){
+        if(res.size()==nums.length){
+            ans.add(new ArrayList<>(res));
+            return;
+        }
+
+        for(int i=0;i<nums.length; i++){
+            if(vis[i]==false){
+                vis[i]=true;
+                res.add(nums[i]);
+                helper(nums, vis, ans, res);
+                res.remove(res.size()-1);
+                vis[i]=false;
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> res= new ArrayList<>();
+        boolean[] vis = new boolean[n];
+        helper(nums, vis, ans , res);
+        return ans;
+    }
+}
