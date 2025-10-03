17ce13886eadf8a7b97d8ae1d851ffaa467d9177
YashikaVashisht <137279767+YashikaVashisht@users.noreply.github.com>
2025-10-03 13:32:47 +0530

    Time: 1 ms (99.77%) | Memory: 47.6 MB (69.41%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,39 +1,24 @@
 class Solution {
-     public static int helper(int i, int j, int[][] grid,  int[][] dp){
-        if(i==0 && j==0) return grid[i][j];
-        if (i<0 || j<0) return (int)1e9;
+    public int helper(int i, int j , int[][] grid, int[][] dp){
+        if(i<0 || j<0) return Integer.MAX_VALUE;;
+        if(i==0 && j==0) return grid[0][0];
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+        if(dp[i][j]!= -1) return dp[i][j];
 
-        int up= grid[i][j]+helper(i-1, j, grid, dp);
-        int left= grid[i][j]+helper(i, j-1, grid, dp);
-        return dp[i][j]= Math.min(up,left);
+        int up= helper(i-1, j , grid, dp);
+        int left= helper(i, j-1, grid, dp);
+
+        return dp[i][j]= Math.min(up, left)+grid[i][j];
     }
-    
     public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][] dp = new int [m][n];
-        for(int[] i:dp){
-            Arrays.fill(i,-1);
+        int n= grid.length;
+        int m= grid[0].length;
+
+        int[][] dp = new int[n][m];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
         }
-        return helper(m-1,n-1,grid, dp);
-        
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(i==0 &&j==0){
-//                     dp[i][j]=grid[i][j];
-//                 }else if(i==0){
-//                     dp[i][j]=dp[i][j - 1] + grid[i][j]; ;
-//                 }else if(j==0){
-//                     dp[i][j]=dp[i - 1][j] + grid[i][j];;
-//                 }else{
-//                     dp[i][j]=Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]; 
-//                 }
-//             }
-//         }
-//         return dp[m - 1][n - 1];
+        return helper(n-1,m-1, grid, dp);
+
     }
 }
