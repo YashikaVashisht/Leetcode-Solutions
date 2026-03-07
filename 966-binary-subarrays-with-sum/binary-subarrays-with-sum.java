39716cd9ab6d09c9d30e899b90422369209428c2
YashikaVashisht <137279767+YashikaVashisht@users.noreply.github.com>
2026-03-07 20:18:40 +0530

    Time: 3 ms (53.35%) | Memory: 51 MB (50.88%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,22 +1,51 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return countAtMost(nums, goal) - countAtMost(nums, goal - 1);
-    }
+        // return countAtMost(nums, goal) - countAtMost(nums, goal - 1);
 
-    private int countAtMost(int[] nums, int goal) {
-        if (goal < 0) return 0;
-        int l = 0;
-        int sum = 0;
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+        int currsum = 0;
+        int countZero = 0;
         int count = 0;
 
-        for (int r = 0; r < nums.length; r++) {
-            sum += nums[r];
-            while (sum > goal) {
-                sum -= nums[l];
-                l++;
+        while (j < n) {
+            currsum += nums[j];
+
+            while (i < j && (nums[i] == 0 || currsum > goal)) {
+                if (nums[i] == 0) {
+                    countZero++;
+                } else {
+                    countZero = 0;
+                }
+                currsum -= nums[i];
+                i++;
             }
-            count += (r - l + 1);
+
+            if (currsum == goal) {
+                count += 1 + countZero;
+            }
+
+            j++;
         }
+
         return count;
     }
+
+    // private int countAtMost(int[] nums, int goal) {
+    //     if (goal < 0) return 0;
+    //     int l = 0;
+    //     int sum = 0;
+    //     int count = 0;
+
+    //     for (int r = 0; r < nums.length; r++) {
+    //         sum += nums[r];
+    //         while (sum > goal) {
+    //             sum -= nums[l];
+    //             l++;
+    //         }
+    //         count += (r - l + 1);
+    //     }
+    //     return count;
+    // }
 }
