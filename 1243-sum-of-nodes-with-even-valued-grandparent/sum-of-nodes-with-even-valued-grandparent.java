2a6195b55d8864dd35ce36b99493701ad442cf25
YashikaVashisht <137279767+YashikaVashisht@users.noreply.github.com>
2026-06-28 13:21:57 +0530

    Time: 1 ms (100.00%) | Memory: 47.5 MB (78.85%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -16,29 +16,28 @@
 class Solution {
     int sum;
     public void helper(TreeNode root){
-        if(root==null) return ;
-        if(root.val%2 ==0){
-            if(root.left!=null){
-                if(root.left.left!=null){
-                    sum+=root.left.left.val;
-                }
-                if(root.left.right!=null){
-                    sum+=root.left.right.val;
-                }
+       if(root==null) return;
+       if(root.val%2==0){
+        if(root.left!=null){
+            if(root.left.left!=null){
+                sum+=root.left.left.val;
             }
-            if(root.right!=null){
-                if(root.right.left!=null){
-                    sum+=root.right.left.val;
-                }
-                if(root.right.right!=null){
-                    sum+=root.right.right.val;
-                }
+            if(root.left.right!=null){
+                sum+=root.left.right.val;
             }
         }
-    
+        if(root.right!=null){
+            if(root.right.left!=null){
+                sum+=root.right.left.val;
+            }
+            if(root.right.right!=null){
+                sum+=root.right.right.val;
+            }
+        }
+       }
+       helper(root.left);
+       helper(root.right);
 
-        helper(root.left);
-        helper(root.right);
     }
     public int sumEvenGrandparent(TreeNode root) {
         sum=0;
