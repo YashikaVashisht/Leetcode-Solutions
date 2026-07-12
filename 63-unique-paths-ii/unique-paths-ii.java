f42df7e0b331c377bc251cc8e2aa71dee6065a89
YashikaVashisht <137279767+YashikaVashisht@users.noreply.github.com>
2026-07-12 10:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (11.53%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,26 +1,24 @@
 class Solution {
     public int helper(int i , int j,  int[][] dp, int[][] grid){
         if(i==0 && j==0) return 1;
-        if(i<0 || j< 0) return 0;
-        if(grid[i][j] == 1) return 0;//obstacle
-
+        if(i<0 ||j <0) return 0;
+        
+        if(grid[i][j]==1) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        int up=helper(i-1,j, dp, grid);
-        int left=helper(i,j-1, dp, grid);
+        int left= helper(i,j-1, dp , grid);
+        int up= helper(i-1,j, dp , grid);
         return dp[i][j]= up+left;
     }
     public int uniquePathsWithObstacles(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-
-        if(grid[0][0]==1 ) return 0;
-
-
-        int[][] dp = new int[m][n];
+        int n= grid.length;
+        int m= grid[0].length;
+        if(grid[0][0] ==1) return 0; //means starting is obsactle already , so no path is possbile 
+        int[][] dp = new int[n][m];
         for(int[] row: dp){
             Arrays.fill(row,-1);
-        }
-        return helper(m-1,n-1, dp, grid);
+         }
+        return helper(n-1,m-1,dp,grid);
+
     }
 }
