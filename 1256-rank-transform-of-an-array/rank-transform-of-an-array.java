56df863c63eabe9e4aac8cf1f3db9ad7abc4428a
YashikaVashisht <137279767+YashikaVashisht@users.noreply.github.com>
2026-07-12 10:42:27 +0530

    Time: 30 ms (71.06%) | Memory: 76.8 MB (42.82%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, Integer> rankMap = new HashMap<>();
+        int rank = 1;
+        for (int num : sorted) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank++);
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rankMap.get(arr[i]);
+        }
+
+        return arr;
+    }
+}
