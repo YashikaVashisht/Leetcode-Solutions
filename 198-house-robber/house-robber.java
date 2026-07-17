d1a5f6851eeddd0e945e947757d7004a3a3160ca
YashikaVashisht <137279767+YashikaVashisht@users.noreply.github.com>
2026-07-17 14:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.42%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,16 @@
 class Solution {
-     public int helper(int[] arr, int sum, int i,  int[] dp){
-        if (i < 0) return 0; 
-        if (i == 0) return arr[0]; 
-        
-        if (dp[i] != -1) return dp[i];
-        int pick = arr[i]+ helper(arr, sum, i-2, dp);
-
-        
-        int notpick = helper(arr, sum , i-1, dp);
-        return dp[i]= Math.max(pick, notpick);
+    int n;
+    public int helper(int[] nums, int i, int[] dp){
+        if(i>=n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int pick= nums[i]+ helper(nums, i+2, dp);
+        int nopick= helper(nums, i+1,  dp);
+        return dp[i]= Math.max(pick,nopick);
     }
-    public int rob(int[] arr) {
-        
-         int n = arr.length;
-        int sum=0;
-        
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return helper(arr, sum, n-1, dp);
-        
+    public int rob(int[] nums) {
+        n= nums.length;
+        int[] dp= new int[n+1];
+        Arrays.fill(dp ,-1);
+        return helper(nums,0, dp);
     }
 }
